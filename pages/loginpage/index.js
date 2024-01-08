c40ee1d08ab274992d7e06d181554a0e5b3afaf4
OAntonov1985 <aantonov1985@gmail.com>
2024-01-08 08:22:02 +0200

    08.01

--- pages/loginpage/index.js
+++ pages/loginpage/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import Image from 'next/image';
 import SingIn from '@/components/SingIn/SingIn';
+import bg from "@/public/vector1.svg"
 
 
 
 export default function LogInPage() {
     return (
-        <div className='login-page'>
+        <>
+
+            <Image
+                alt="Mountains"
+                src={bg}
+                // placeholder="blur"
+                quality={100}
+                fill
+                sizes="100%, 100%"
+                style={{
+                    objectFit: 'cover',
+                    zIndex: -1,
+                    // width: 100vw,
+                    // backgroundPosition: .55, top
+                    // backgroundSize: 100 % auto
+                }}
+            />
             <SingIn />
-        </div>
+        </>
 
     );
 }
