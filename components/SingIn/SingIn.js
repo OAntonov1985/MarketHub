423881bfb8d4f2c865cfa648924026f6ba349cbc
OAntonov1985 <aantonov1985@gmail.com>
2024-01-09 18:06:33 +0200

    09.01

--- components/SingIn/SingIn.js
+++ components/SingIn/SingIn.js
@@ -4,8 +4,11 @@ import { useState } from 'react';
 import login from '../../pages/api/auth'
 import axios from 'axios';
 // import { FormEvent } from 'react'
-const url = 'https://markethub-mfbw.onrender.com/markethub/users/8';
-const token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJub3R5b3VyYnVzaW5lc3R0LmdtYWlsLmNvbSIsImlzcyI6InRva2VuIiwiaWF0IjoxNzA0ODEzMzE2LCJleHAiOjE3MDQ4OTk3MTZ9.C68eqXFlqQoi5eX_Q2Ndvq-OOqLH78nDPLV-_N6FLcNkiitR65XZOulBSxn1N_b98CoiqWNLAePo6O4zVZnHlA';
+const url = 'https://markethub-mfbw.onrender.com/markethub/login';
+const data = {
+    email: 'notyourbusinestt.gmail.com',
+    password: 'pass777333'
+};
 
 
 export default function SingIn({ toggleMode }) {
@@ -14,13 +17,20 @@ export default function SingIn({ toggleMode }) {
         event.preventDefault();
         console.log('go')
         try {
-            const response = await axios.get(url, {
+            const response = await fetch(url, {
+                method: 'POST',
                 headers: {
-                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json',
                 },
+                body: JSON.stringify(data),
             });
 
-            console.log('Успех:', response.data);
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const responseData = await response.json();
+            setResponseMessage(responseData);
         } catch (error) {
             console.error('Ошибка:', error);
         }
