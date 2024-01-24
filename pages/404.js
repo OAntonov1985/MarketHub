8725b0caf0ae05b9ac70b61e16d9e0f1b6b2c0ac
OAntonov1985 <aantonov1985@gmail.com>
2024-01-24 20:37:10 +0200

    24.01

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,39 @@
-import { useEffect } from "react";
+import Image from 'next/image';
+import Link from 'next/link';
 import { useRouter } from "next/router";
 
 export default function Error404() {
     const router = useRouter();
 
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //         router.push("/");
-    //     }, 3000);
-    // }, [router]);
-    return <>Упс. Щось пішло не так. Сторінка не знайдена</>;
+
+    return (
+        <div className='error-page'>
+            <div className="error-page-left-column">
+                <div className="error-page-image-container">
+                    <Image
+                        alt="image of computer"
+                        src='/errorImage.png'
+                        quality={100}
+                        fill
+                        style={{
+                            objectFit: 'contain',
+                            width: '100%'
+                        }}
+                    />
+                </div>
+            </div>
+
+            <div className="error-page-right-column">
+                <p className='error-page-404'>404</p>
+                <p className='error-page-text-alert'>Ой! Схоже, ви загубилися в цифровому просторі. Сторінку, яку ви шукали, не знайдено</p>
+                <Link href='/'>
+                    <div className='error-page-div-for-button'>
+                        <button className='error-page-button-tomain'>Головна сторінка</button>
+                    </div>
+
+                </Link>
+
+            </div>
+        </div>
+    );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import "@/styles/globals.css";
-import { Ubuntu } from 'next/font/google';
+import { Ubuntu, Poppins } from 'next/font/google';
 
 const ubuntu = Ubuntu({
     subsets: ['cyrillic'],
     weight: ["300", "400", "500", '700'],
 })
 
+const poppins = Poppins({
+    subsets: ['latin'],
+    weight: ["300", "400", "500", '700'],
+});
 export default function MyApp({ Component, pageProps }) {
     return (
         <div className={ubuntu.className}>
