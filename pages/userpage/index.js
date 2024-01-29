a72426ee8260ed63b6fc4db26fd00e14118df66e
OAntonov1985 <aantonov1985@gmail.com>
2024-01-29 09:53:37 +0200

    29.01

--- pages/userpage/index.js
+++ pages/userpage/index.js
@@ -1,8 +1,12 @@
+"use client";
+
+
 import { useEffect } from 'react';
 import Cookies from 'js-cookie';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import Header from '@/components/Header/Header';
+import Footer from '@/components/Footer/Footer';
 import GoodCard from '@/components/GoodCard/GoodCard';
 
 
@@ -20,8 +24,8 @@ export default function UserPage() {
             {/* <GoodCard /> */}
             <Header />
             <p>Your Profile</p>
-            <div>привіт {userName}</div>
-            <pre>{JSON.stringify(user, null, 2)}</pre>
+            {/* <div>привіт {userName}</div>
+            <pre>{JSON.stringify(user, null, 2)}</pre> */}
             <Link href='/mainpage/' className='main-link'>
                 <p className='main-link'>Головна сторінка з категоріями і пошуком</p>
             </Link>
@@ -29,6 +33,7 @@ export default function UserPage() {
                 <p className='main-link'>Головна сторінка</p>
             </Link>
             <button onClick={deleteToken}>Вийти з акаунту</button>
+            <Footer />
         </>
     )
 }
