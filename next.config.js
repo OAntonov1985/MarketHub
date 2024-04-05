98c50f309fc6d8041e0bc2d03136cfb1d8d92fb8
OAntonov1985 <aantonov1985@gmail.com>
2024-04-05 19:18:50 +0300

    05.04

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        domains: ["markethub-mfbw.onrender.com", "storage.googleapis.com", "brain.com.ua", "netlify.com", "markethubstore.netlify.app"],
+        domains: ["markethub-mfbw.onrender.com", "storage.googleapis.com", "brain.com.ua", "netlify.com", "markethubstore.netlify.app", "firebasestorage.googleapis.com"],
     },
 };
 
