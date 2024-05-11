2de02a6890b6f79470a2d084290476f8523e117b
OAntonov1985 <aantonov1985@gmail.com>
2024-05-11 13:20:56 +0300

    11.05

--- config/config.js
+++ config/config.js
@@ -1,36 +1,36 @@
-import GoogleProvider from 'next-auth/providers/google'
-import FacebookProvider from "next-auth/providers/facebook";
-// import { users } from '../config/user';
+// import GoogleProvider from 'next-auth/providers/google'
+// import FacebookProvider from "next-auth/providers/facebook";
+// // import { users } from '../config/user';
 
-export const authConfig = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            authorization: {
-                params: {
-                    scope: "openid email profile https://www.googleapis.com/auth/drive.file" // Объединяем scope
-                }
-            },
-            idToken: true, // Включаем получение idToken
-            checks: ["pkce", "state"],
-            profile(profile) {
-                return {
-                    id: profile.sub,
-                    name: profile.name,
-                    email: profile.email,
-                    image: profile.picture,
-                }
-            },
-        }),
-    ],
-    cookies: true
-}
+// export const authConfig = {
+//     providers: [
+//         GoogleProvider({
+//             clientId: process.env.GOOGLE_CLIENT_ID,
+//             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//             authorization: {
+//                 params: {
+//                     scope: "openid email profile https://www.googleapis.com/auth/drive.file" // Объединяем scope
+//                 }
+//             },
+//             idToken: true, // Включаем получение idToken
+//             checks: ["pkce", "state"],
+//             profile(profile) {
+//                 return {
+//                     id: profile.sub,
+//                     name: profile.name,
+//                     email: profile.email,
+//                     image: profile.picture,
+//                 }
+//             },
+//         }),
+//     ],
+//     cookies: true
+// }
 
 
-// FacebookProvider({
-//     clientId: process.env.FACEBOOK_CLIENT_ID,
-//     clientSecret: process.env.FACEBOOK_CLIENT_SECRET
-// })
+// // FacebookProvider({
+// //     clientId: process.env.FACEBOOK_CLIENT_ID,
+// //     clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+// // })
 
 
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,25 +1,57 @@
-// 
+import { connectMongoDB } from "../../../config/mongodb";
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+import mongoose from "mongoose";
 
-// import { authConfig } from '../../../config/config';
-// import NextAuth from 'next-auth'
+const authOptions = {
 
-// export default (req, res) => NextAuth(req, res, authConfig);
-
-import GoogleProvider from 'next-auth/providers/google'
-
-import NextAuth from "next-auth"
-
-export const authOptions = {
-    // secret: process.env.NEXTAUTN_SECRET,
     providers: [
+        process.env.VERCEL_ENV === "preview",
         GoogleProvider({
+            checks: ['none'],
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            checks: ['none'],
-            cookies: true
+            authorization: { params: { scope: "openid email profile" } },
+            idToken: true,
+            // checks: ["pkce", "state"]
         }),
-
     ],
-    cookies: true
-}
-export default NextAuth(authOptions)
+    callbacks: {
+        async signIn({ user, account }) {
+            if (account.provider === "google") {
+                const { name, email } = user;
+
+                const userInf0 = {
+                    name: name.split(' ')[0],
+                    surname: name.split(' ')[1],
+                    nameAs: {
+                        nameAs: name.split(' ')[0],
+                        surnameAs: name.split(' ')[1],
+                    },
+                    email,
+                    userOrders: {},
+                    userProductsToSale: {}
+                }
+
+                try {
+                    await connectMongoDB();
+                    const userExists = await mongoose.connection.collection("users").findOne({ email });
+
+                    if (!userExists) {
+                        await mongoose.connection.collection("users").insertOne(userInf0);
+                        console.log("User saved successfully:", userInf0);
+                    }
+
+                    return user;
+                } catch (error) {
+                    console.error("Error processing signIn callback:", error);
+                }
+            }
+
+            return user;
+        },
+    },
+    secret: process.env.NEXTAUTN_SECRET
+};
+
+export default NextAuth(authOptions);
