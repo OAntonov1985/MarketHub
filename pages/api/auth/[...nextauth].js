006a6a8c3984c8096d1025aa3483a749d013414b
OAntonov1985 <aantonov1985@gmail.com>
2024-06-13 16:01:27 +0300

    13.06/5

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -15,6 +15,7 @@ const authOptions = (req, res) => {
             strategy: "jwt",
         },
         providers: [
+            process.env.VERCEL_ENV === "preview",
             GoogleProvider({
                 name: "google",
                 clientId: process.env.GOOGLE_CLIENT_ID,
@@ -31,66 +32,53 @@ const authOptions = (req, res) => {
                     const { email, password } = credentials;
                     try {
                         await connectMongoDB();
+                        const userExists = await mongoose.connection.collection("users").findOne({ email, password });
 
-                        if (!mongoose.connection.readyState) {
-                            throw new Error("Помилка підключення до бази даних");
+                        if (userExists.email == email && userExists.password == password) {
+                            return userExists;
                         }
-                        const userExists = await mongoose.connection.collection("users").findOne({ email });
-                        if (userExists) {
-                            if (userExists.email === email && userExists.password === password) {
-                                const { nameAs, email, password, pfone } = userExists;
-                                console.log(nameAs.nameAs)
-                                const cookies = [
-                                    serialize('userName', nameAs.nameAs, { path: '/' }),
-                                    serialize('userSurname', nameAs.surnameAs, { path: '/' }),
-                                    serialize('userEmail', email, { path: '/' }),
-                                    serialize('userPassword', password, { path: '/' }),
-                                    // serialize('userToken', token, { path: '/' }),
-                                    // pfone.length > 0 ? serialize('userPhone', pfone, { path: '/' }) : null
-                                ].filter(Boolean);
-
-                                res.setHeader('Set-Cookie', cookies);
-                                return userExists;
-                            } else {
-                                console.log("Помилка авторизакії!");
-                                return null;
-                            }
-                        } else {
-                            console.log("Authorization failed: user not found");
-                            return null;
+                        else if (userExists.email == email && userExists.password !== password) {
+                            return null
                         }
+                        else if (!userExists) {
+                            return new Error("Користувача з такою поштою не знайдено.")
+                        }
+                        return null;
                     } catch (error) {
-                        console.error("Error in authorize function:", error);
-                        throw new Error("An error occurred while processing your request.");
+                        console.error("Error processing signIn callback:", error);
                     }
+                    return null
                 }
             })
         ],
-
 
         callbacks: {
             async signIn({ user, account }) {
                 if (account.provider === "google" || account.provider === "facebook") {
                     const { name, email } = user;
-                    // const userInf0 = {
-                    //     name: name.split(' ')[0],
-                    //     surname: name.split(' ')[1] || '',
-                    //     nameAs: {
-                    //         nameAs: name.split(' ')[0],
-                    //         surnameAs: name.split(' ')[1] || '',
-                    //     },
-                    //     email,
-                    //     userOrders: {},
-                    //     userProductsToSale: {},
-                    //     password: "password",
-                    //     pfone: ""
-                    // };
+                    // const customArg = context.query.customArg;
+                    // console.log(customArg)
+                    // console.log(profile)
+                    const userInf0 = {
+                        name: name.split(' ')[0],
+                        surname: name.split(' ')[1],
+                        nameAs: {
+                            nameAs: name.split(' ')[0],
+                            surnameAs: name.split(' ')[1],
+                        },
+                        email,
+                        userOrders: {},
+                        userProductsToSale: {},
+                        password: "password",
+                        pfone: ""
+                    }
 
                     try {
                         await connectMongoDB();
                         const userExists = await mongoose.connection.collection("users").findOne({ email });
 
                         if (userExists) {
+                            // console.log(userExists)
                             const { nameAs, email, password, pfone } = userExists;
                             const cookies = [
                                 serialize('userName', nameAs.nameAs, { path: '/' }),
@@ -103,26 +91,68 @@ const authOptions = (req, res) => {
 
                             res.setHeader('Set-Cookie', cookies);
                             return userExists;
-
-                        } else {
-                            return null;
                         }
+                        else if (!userExists) {
+                            await mongoose.connection.collection("users").insertOne(userInf0);
+                            return userInf0;
+                            // return null;
+                        }
+                        return null;
                     } catch (error) {
                         console.error("Error processing signIn callback:", error);
-                        return null;
                     }
+                    // return user;
                 }
-                return true;
+                // else if (account.provider === "google1") {
+                //     const { name, email } = user;
+                //     console.log("google1")
+
+
+                //     const userInf0 = {
+                //         name: name.split(' ')[0],
+                //         surname: name.split(' ')[1],
+                //         nameAs: {
+                //             nameAs: name.split(' ')[0],
+                //             surnameAs: name.split(' ')[1],
+                //         },
+                //         email,
+                //         userOrders: {},
+                //         userProductsToSale: {},
+                //         password: "password",
+                //         pfone: ""
+                //     }
+
+                //     try {
+                //         await connectMongoDB();
+                //         const userExists = await mongoose.connection.collection("users").findOne({ email });
+
+                //         if (userExists) {
+                //             return null;
+
+                //         }
+                //         else if (!userExists) {
+                //             await mongoose.connection.collection("users").insertOne(userInf0);
+                //             return userInf0;
+                //             // return userExists;
+                //         }
+                //         return null;
+                //     } catch (error) {
+                //         console.error("Error processing signIn callback:", error);
+                //     }
+                //     return userInf0;
+                // }
+
+
             },
-            secret: process.env.NEXTAUTH_SECRET
         },
-        secret: process.env.NEXTAUTH_SECRET
+        // pages: {
+        //     signIn: '/loginpage',
+        //     error: '/error'
+        // },
+        secret: process.env.NEXTAUTN_SECRET
     };
-    secret: process.env.NEXTAUTH_SECRET
-};
-secret: process.env.NEXTAUTH_SECRET
 
-
+}
 
 
 export default (req, res) => {
